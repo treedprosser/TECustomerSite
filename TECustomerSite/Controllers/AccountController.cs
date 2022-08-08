ae339fe0df4cb93a746cbc448814151a25b78de0
TorresLino <arthur.torres.de.lino@gmail.com>
2022-08-08 17:50:39 -0600

    Authentication

---
+++ TECustomerSite/Controllers/AccountController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using TECustomerSite.Models;
+
+namespace TECustomerSite.Controllers
+{
+	public class AccountController : Controller
+	{
+		public IActionResult Login( string? returnUrl = null)
+		{
+			if (returnUrl != null)
+				TempData["returnUrl"] = returnUrl;
+			return View();
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> LoginAsync( Customer customer)
+		{
+			TravelExpertsContext db = new TravelExpertsContext();
+			var usr = db.Customers
+				.Where( c => c.Username == customer.Username && c.Password == customer.Password )
+				.FirstOrDefault();
+
+			if (usr == null)
+				return View();
+
+			var claims = new List<Claim>()
+			{
+				new Claim(ClaimTypes.Name, usr.Username),
+				new Claim("FirstName", usr.CustFirstName)
+			};
+
+			var claimsIdentity = new ClaimsIdentity(claims, "Cookies");
+
+			await HttpContext.SignInAsync("Cookies", new ClaimsPrincipal(claimsIdentity));
+
+			if (TempData["ReturnUrl"] == null)
+				return RedirectToAction("Index", "Home"); //change this depending on the final version of the project
+			else
+				return Redirect(TempData["ReturnUrl"].ToString());
+		}
+
+		public async Task<IActionResult> LogoutAsync()
+		{
+			await HttpContext.SignOutAsync("Cookies");
+			return RedirectToAction("Index", "Home");
+		}
+	}
+}
