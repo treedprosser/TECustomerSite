8904f597d1d342a86ed8b806c2ba5139abddaf7c
trevor <treedprosser@gmail.com>
2022-08-08 14:45:33 -0600

    Add registration page

---
+++ TECustomerSite/Controllers/AccountController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TECustomerSite.Models;
+
+namespace TECustomerSite.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Register()
+        {
+            return View(new Customer());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Register(Customer customer)
+        {
+            try
+            {
+                CustomerManager.Register(customer);
+                return RedirectToAction("Index", "Home");
+            }
+            catch
+            {
+                TempData["IsError"] = true;
+                TempData["Message"] = "Error registering, please try again later";
+                return View();
+            }
+        }
+    }
+}
---
+++ TECustomerSite/Models/CustomerManager.cs
@@ -0,0 +1,12 @@
+﻿namespace TECustomerSite.Models
+{
+    public class CustomerManager
+    {
+        public static void Register(Customer customer)
+        {
+            TravelExpertsContext db = new TravelExpertsContext();
+            db.Customers.Add(customer);
+            db.SaveChanges();
+        }
+    }
+}
