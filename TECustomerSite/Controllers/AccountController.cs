e45e3eb73dc69dc055ccb5b8e9a1b1fd44424f92
brianna <brianna.gibson@edu.sait.ca>
2022-08-10 10:26:23 -0600

    CustomerController

--- TECustomerSite/Controllers/AccountController.cs
+++ TECustomerSite/Controllers/AccountController.cs
@@ -78,35 +78,5 @@ namespace TECustomerSite.Controllers
                 return View();
             }
         }
-
-        [Authorize]
-		public IActionResult Edit(int id)
-        {
-            Customer customer = CustomerManager.FindCustomer(id);
-            return View(customer);
-        }
-
-        [HttpPost]
-        [Authorize]
-        [ValidateAntiForgeryToken]
-		public IActionResult Edit(int id, Customer newData)
-        {
-            if (HttpContext.Session.GetInt32("CurrentCustomer") > 0)
-            {
-                id = HttpContext.Session.GetInt32("CurrentCustomer")??0;
-            }
-            try
-            {
-                CustomerManager.UpdateCustomer(id, newData);
-                TempData["Message"] = $"Successfully updated {newData.CustFirstName}'s information";
-                return RedirectToAction(nameof(Index));
-            }
-            catch 
-            {
-				TempData["IsError"] = true;
-				TempData["Message"] = "Error when attempting to edit Customer";
-				return View();
-            }
-        }
     }
 }
