ee6e810c97ecd5121aeda5bc4348137225c79826
7ilmi <105809829+7ilmi@users.noreply.github.com>
2022-08-10 10:54:24 -0600

    Update BookingsController.cs

--- TECustomerSite/Controllers/BookingsController.cs
+++ TECustomerSite/Controllers/BookingsController.cs
@@ -13,7 +13,7 @@ namespace TECustomerSite.Controllers
             int? customerID = HttpContext.Session.GetInt32("CurrentCustomer");
             // Could create a BookingsManager class and add a method to it that will grab all the bookings for a given customer ID
             // Then call the method on the list created above, something like what's written below
-            // bookings = BookingsManager.GetBookingsByCustomer((int)customerID);
+            //bookings = BookingManager.GetBookingsByCustomer((int)customerID);
             return View(bookings);
         }
     }
